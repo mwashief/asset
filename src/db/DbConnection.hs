f9767f5e9ed92288f89db9c3247195d3efb3755e
Washief Mugdho <muwashief@gmail.com>
2023-12-13 05:42:15 +0600

    Cred info

--- src/db/DbConnection.hs
+++ src/db/DbConnection.hs
@@ -9,5 +9,7 @@ module DbConnection where
 
 import Hasql.Connection (Connection, ConnectionError, acquire, settings)
 
+-- | Dummy connection credentials data
+--   which can be captured from env
 getConnection :: IO (Either ConnectionError Connection)
 getConnection = acquire $ settings "localhost" 5432 "postgres" "postgres" "postgres"
